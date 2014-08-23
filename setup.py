6b9c5c61efa381709099a2f6a0ced5625d8d222f
Federico Reiven <reiven@gmail.com>
2014-08-23 15:58:34 -0300

    added first version of setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+requires = ['']
+
+
+setup(name='tomber',
+    version='1.0.1',
+    description='a python Tomb (the Crypto Undertaker) wrapper',
+    long_description=read('README.md'),
+    license="BSD",
+    author='Federico Reiven',
+    author_email='reiven@gmail.com',
+    url='https://github.com/reiven/tomber',
+    keywords='tomb wrapper',
+    packages=find_packages(),
+    install_requires=requires,
+    )
